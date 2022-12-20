e5ce76a3746c4a9a498b95f0953988532273b032
FedirTikhonov <fedir.tikhonov@gmail.com>
2022-12-20 11:06:31 +0100

    Update indexes.sql

--- kursach/structure and data/indexes.sql
+++ kursach/structure and data/indexes.sql
@@ -1,8 +1,6 @@
-EXPLAIN SELECT * FROM clients_to_optimise WHERE name = 'client_name_1999';
+CREATE INDEX client_name_scan ON clients(passport);
 
-CREATE INDEX client_name_scan ON clients_to_optimise(name);
-
-EXPLAIN SELECT * FROM clients_to_optimise WHERE passport = '111111111';
+EXPLAIN SELECT * FROM clients WHERE passport = '111111111';
 
 DROP INDEX client_name_scan;
 
